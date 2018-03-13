48cd2472c6e9c0d1e7c2f575a725e99eedce8e83
Enoch <jenoch@proforma.com>
2018-03-12 22:45:39 -0400

    forgot to deploy

--- public/main.js
+++ public/main.js
@@ -1,5 +1,46 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+let total1 = 0;
+let total2 = 0;
+console.log(total1);
+
+  const t1add1 = () => {
+    total1++;
+    console.log(total1)
+    const teamscore1 = document.querySelector(".teamonescore");
+    teamscore1.textContent = total1;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const t2add1 = () => {
+  total2++;
+  console.log(total1)
+  const teamscore2 = document.querySelector(".teamtwoscore");
+  teamscore2.textContent = total2;
+}
+
+const t1sub1 = () => {
+  total1--;
+  console.log(total1)
+  const teamscore1 = document.querySelector(".teamonescore");
+  teamscore1.textContent = total1;
+}
+
+const t2sub1 = () => {
+  total2--;
+  console.log(total1)
+  const teamscore2 = document.querySelector(".teamtwoscore");
+  teamscore2.textContent = total2;
+}
+
+const updatet1 = () => {
+  let updateTeamOne = document.querySelector(".rename1").value;
+  console.log(updateTeamOne);
+  const finalTeamName = document.querySelector(".renamedteam1")
+  finalTeamName.textContent = updateTeamOne
+}
+
+const updatet2 = () => {
+  let updateTeamTwo = document.querySelector(".rename2").value;
+  console.log(updateTeamTwo);
+  const finalTeamName = document.querySelector(".renamedteam2")
+  finalTeamName.textContent = updateTeamTwo
+}
+
